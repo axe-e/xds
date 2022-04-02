49fbe5ad5ba6ada4d904842ef5fe4ac826226315
axe-e <102362908+axe-e@users.noreply.github.com>
2022-04-02 08:57:30 +0530

    Update config.sh

--- CI_script/config.sh
+++ CI_script/config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/axe-e/xds.git -b main"
+export DT_LINK="cd /root/work/out/target/product/spes/ -b fox_11.0"
 
 export DEVICE="spes"
 export OEM="xiaomi"
